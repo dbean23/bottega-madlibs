09e75ddb9c99acee739aac721e3fc7adef623e1d
David Bean <dbean23@gmail.com>
2018-12-31 16:25:02 -0700

    changed port and app title

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Bootstrap</h2>
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div className = 'app' >
+            <
+            h1 > Bottega Madlibs < /h1> <
+            h2 > React Bootstrap < /h2> <
+            /div>
+        );
+    }
 }
